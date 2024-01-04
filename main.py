d68ae34550c480e7918b805ec095f8a71db3ed08
tvernier21 <thibaultvernier13@gmail.com>
2024-01-03 21:41:58 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+import numpy as np
+import spotify_util as spotify
+
+search = "2016 rap"
+
+print("Getting songs from playlists...")
+playlists = spotify.get_playlist_from_search(search, limit=15)
+tracks = [spotify.get_tracks_from_playlist(playlist) for playlist in playlists]
+
+
+all_tracks = [track for subtrack in tracks for track in subtrack]
+unique_tracks = set(all_tracks)
+print(f"Found {len(unique_tracks)} unique tracks")
+
+# Create a dictionary of track ids to indices
+track_dict = {track: i for i, track in enumerate(unique_tracks)}
+# Create a nxn graph representation of edges between tracks
+edge_graph = np.zeros((len(unique_tracks), len(unique_tracks)))
+# Create a track frequency dictionary
+track_freq = {track: 0 for track in unique_tracks}
+
+print("Calculating edges...")
+# For each track, get every other track in the playlist and increment the edge
+for track_list in tracks:
+    for track in track_list:
+        track_freq[track] += 1
+        for other_track in track_list:
+            if track != other_track:
+                edge_graph[
+                    track_dict[track],
+                    track_dict[other_track]
+                ] += 1
+
+# Get the most frequent tracks
+most_freq = sorted(track_freq.items(), key=lambda x: x[1], reverse=True)
+top_10 = most_freq[:10]
+top_10_names = [spotify.get_song_features(track[0])["name"] for track in top_10]
+
+print("Calculating similarity...")
+# of the top 10, determine each of those top 10 most similar
+similarity = []
+for track_id, _ in top_10:
+    similarity.append(
+        sorted(
+            [(other_track, edge_graph[track_dict[track_id], track_dict[other_track]]) for other_track in unique_tracks],
+            key=lambda x: x[1],
+            reverse=True
+        )[:10]
+    )
+
+# Print the results
+for i, track in enumerate(top_10_names):
+    print(f"Track: {track}")
+    for j, (track_id, w) in enumerate(similarity[i]):
+        print(f"\t{j+1}. {spotify.get_song_features(track_id)['name']} ({w})")
+    print()
+
+
+
---
+++ spotify_util.py
@@ -0,0 +1,88 @@
+import requests
+import os
+from dotenv import load_dotenv
+import pandas as pd
+
+SPOTIFY_URL = "https://api.spotify.com/v1/"
+
+def get_client_credentials():
+    load_dotenv()
+    client_id = os.getenv("SPOTIFY_CLIENT_ID")
+    client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+    return client_id, client_secret
+
+def get_api_headers():
+    client_id, client_secret = get_client_credentials()
+
+    url = "https://accounts.spotify.com/api/token"
+    auth_response = requests.post(url, {
+        "grant_type": "client_credentials",
+        "client_id": client_id,
+        "client_secret": client_secret,
+    })
+    auth_response_json = auth_response.json()
+    access_token = auth_response_json["access_token"]
+    headers = {
+        "Authorization": "Bearer {token}".format(token=access_token) 
+    }
+    return headers
+
+def get_playlist_from_search(search_query, limit=6):
+    res = requests.get(SPOTIFY_URL + "search",
+                       headers=get_api_headers(),
+                       params={
+                           "query": search_query,
+                           "type": "playlist",
+                           "limit": limit,
+                       }).json()
+    if "error" in res:
+        raise Exception(res["error"]["message"])
+        return None
+    playlists = [item["id"] for item in res["playlists"]["items"]]
+    return playlists
+
+def get_tracks_from_playlist(playlist_id):
+    res = requests.get(SPOTIFY_URL + f"playlists/{playlist_id}/tracks",
+                       headers=get_api_headers()).json()
+    if "error" in res:
+        raise Exception(res["error"]["message"])
+        return None
+
+    tracks = []
+    for item in res["items"]:
+        if item["is_local"]:
+            continue
+        try:
+            tracks.append(item["track"]["id"])
+        except:
+            continue
+
+    n = res["total"]
+    for i in range(1, n // 100 + 1):
+        res = requests.get(SPOTIFY_URL + f"playlists/{playlist_id}/tracks",
+                           headers=get_api_headers(),
+                           params={
+                               "offset": i * 100,
+                           }).json()
+        if "error" in res:
+            raise Exception(res["error"]["message"])
+            return None
+
+        for item in res["items"]:
+            if item["is_local"]:
+                continue
+            try:
+                tracks.append(item["track"]["id"])
+            except:
+                continue
+    return tracks
+
+def get_song_features(track_id):
+    res = requests.get(SPOTIFY_URL + f"tracks/{track_id}",
+                       headers=get_api_headers()).json()
+    if "error" in res:
+        raise Exception(res["error"]["message"])
+        return None
+    return res
+
+
